70a9472a127399ca030746caf50c1dcc0a3f46b6
miloszKubiak <milypan69@gmail.com>
2021-12-28 16:43:49 +0100

    create keys, title and add sound assets

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,102 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="title">
+      <i class="fab fa-vuejs"></i>
+      <h1>vue drumkit</h1>
+      <i class="fas fa-drum"></i>
+    </div>
+    
+    <div class="keys">
+      <Key v-for="(key, i) in keys" :sound="key" :key="i" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Key from './components/Key.vue'
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      keys: [
+        {
+          name: "boom",
+          audio: require("./assets/sounds/boom.wav"),
+          letter: "A"
+        },
+        {
+          name: "clap",
+          audio: require("./assets/sounds/clap.wav"),
+          letter: "S"
+        },
+        {
+          name: "hihat",
+          audio: require("./assets/sounds/hihat.wav"),
+          letter: "D"
+        },
+        {
+          name: "kick",
+          audio: require("./assets/sounds/kick.wav"),
+          letter: "F"
+        },
+        {
+          name: "openhat",
+          audio: require("./assets/sounds/openhat.wav"),
+          letter: "G"
+        },
+        {
+          name: "ride",
+          audio: require("./assets/sounds/ride.wav"),
+          letter: "H"
+        },
+        {
+          name: "snare",
+          audio: require("./assets/sounds/snare.wav"),
+          letter: "J"
+        },
+        {
+          name: "tink",
+          audio: require("./assets/sounds/tink.wav"),
+          letter: "K"
+        },
+        {
+          name: "tom",
+          audio: require("./assets/sounds/tom.wav"),
+          letter: "L"
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
-  }
-}
+    Key
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Poppins, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.title {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.title h1 {
+  padding: 20px;
+}
+
+.keys {
+  display: flex;
+  align-items:center;
+  justify-content: center;
 }
 </style>
---
+++ src/components/Key.vue
@@ -0,0 +1,16 @@
+<template>
+  <button :class="`key`">
+    {{ sound.letter }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'key',
+  props: ['sound']
+}
+</script>
+
+<style>
+
+</style>
